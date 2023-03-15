b19c7fbc8536182d78997ea4a3a22199188e082f
Filip <filip.uradnik9@gmail.com>
2023-03-15 22:08:22 +0100

    Fix #28 - Add Shapley value computer.

--- incomplete_cooperative/shapley.py
+++ incomplete_cooperative/shapley.py
@@ -11,19 +11,27 @@ def _get_contributions(number_of_players: int) -> None:
 
 
 def compute_shapley_value(game: IncompleteCooperativeGame,
-                          get_values: Callable = lambda x: x.values) -> None:  # TODO: typehints.
+                          get_values: Callable = lambda x: x.values,
+                          get_values_without: Callable | None = None) -> Value:  # TODO: typehints.
     """Compute the Shapley value.
 
-    The `get_value` function must get a `IncompleteCooperativeGame` and a `Coalition` and return its value.
+    The `get_values` function must get a `IncompleteCooperativeGame` and a `Coalition` and return its value.
     It is done like this to allow flexibility -- sometimes, we want to compute the Shapley value from up/low bounds.
+    Finally, the `get_values_without` is a function that will compute the values of coalitions without the player.
+    It defaults to `get_values` if not specified.
     """
+    if get_values_without is None:
+        get_values_without = get_values
+
     coalition_contribution_coefficients = _get_contributions(game.number_of_players)
     n_fac = factorial(game.number_of_players)
     for player in range(game.number_of_players):
         player_singleton = game.players_to_coalition([player])
         coalitions_without_player = np.fromiter(game.get_coalitions_not_including_players([player]), Value)
 
-        values_without = get_values(game)[coalitions_without_player]
+        values_without = get_values_without(game)[coalitions_without_player]
         values_with = get_values(game)[coalitions_without_player | player_singleton]
-        coalition_contributions = coalition_contribution_coefficients[list(map(game.get_coalition_size, coalitions_without_player))]
+        coalition_contributions = coalition_contribution_coefficients[list(map(game.get_coalition_size,
+                                                                               coalitions_without_player))]
+
         yield np.sum(coalition_contributions * (values_with - values_without)) / n_fac
