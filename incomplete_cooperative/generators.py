8eb1781c2e1107918c33ef8957f10e5dd48dc2f3
Filip <filip.uradnik9@gmail.com>
2023-08-04 21:38:33 +0200

    Fix #61 - Implement subproc vec env.

--- incomplete_cooperative/generators.py
+++ incomplete_cooperative/generators.py
@@ -7,10 +7,14 @@ from .game import IncompleteCooperativeGame
 from .protocols import Game, GameGenerator
 
 
+def _none_bounds_computer(self) -> None:  # pragma: nocover
+    return None
+
+
 def factory_generator(number_of_players: int, owner: int | None = None) -> Game:
     """Generate a `factory` game."""
     owner = randrange(0, number_of_players) if owner is None else owner
-    game = IncompleteCooperativeGame(number_of_players, lambda x: None)
+    game = IncompleteCooperativeGame(number_of_players, _none_bounds_computer)
     for coalition in all_coalitions(game):
         if owner not in coalition:
             game.set_value(0, coalition)
