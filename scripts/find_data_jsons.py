3972be7867f520b27dc09327b1c7a04a7a9f6cc2
Filip <filip.uradnik9@gmail.com>
2023-08-22 22:35:57 +0200

    Fix #148 - Add validator best_is_best.

---
+++ scripts/find_data_jsons.py
@@ -0,0 +1,22 @@
+"""Helper script for finding data.json files in a path."""
+from pathlib import Path
+from typing import Iterator
+
+from incomplete_cooperative.run.save import Output, get_outputs_from_file
+
+
+def find_data_jsons(path: Path) -> Iterator[Path]:
+    """Find all data.json files in the subdirectories."""
+    if (path / "data.json").exists():
+        print(path)
+        yield path / "data.json"
+        return
+
+    for x in path.iterdir():
+        if x.is_dir():
+            yield from find_data_jsons(x)
+
+
+def find_data(path: Path) -> Iterator[dict[str, Output]]:
+    """Find all data.jsons in path, return their parsed outputs."""
+    return map(get_outputs_from_file, find_data_jsons(path))
---
+++ scripts/validator_best_is_best.py
@@ -0,0 +1,31 @@
+"""Validate that best is actually always best."""
+from pathlib import Path
+from sys import argv
+
+import numpy as np
+
+from incomplete_cooperative.run.save import Output
+
+from .find_data_jsons import find_data
+
+EPSILON = 0.000000001
+
+
+def validate_data(data: dict[str, Output]) -> None:
+    """Return output."""
+    if "best_states" not in data.keys():
+        return  # no best, nothing to validate
+    # make them a little better to account for float precision
+    best_exploitabilities = data["best_states"].avg_exploitabilities - EPSILON
+
+    for key, output in data.items():
+        exploitability = output.exploitability
+        for i in range(exploitability.shape[0]):
+            assert np.all(exploitability[i] >= best_exploitabilities[i]), \
+                f"{key}, ex:{exploitability[i].tolist()}, bst:{best_exploitabilities[i]}"
+
+
+def main(path=Path(argv[1])) -> None:
+    """Validate in a path."""
+    for data in find_data(path):
+        validate_data(data)
