17cbfc5294ab4cdb6e6b11a18827db4e5d51cbf2
Filip <filip.uradnik9@gmail.com>
2023-11-24 19:41:26 +0100

    Repair supermodularity check.

--- incomplete_cooperative/supermodularity_check.py
+++ incomplete_cooperative/supermodularity_check.py
@@ -1,13 +1,22 @@
 """Check if a game is supermodular."""
 from .coalitions import Coalition, all_coalitions, grand_coalition
-from .protocols import Game, Player
+from .protocols import Game, IncompleteGame, Player
 
 
-def check_supermodularity(game: Game) -> tuple[Coalition, Coalition, Player] | None:
+def check_supermodularity(game: IncompleteGame | Game) -> tuple[Coalition, Coalition, Player] | None:
     """Return the violating coalitions and player; or None if the game is supermodular."""
     for T in all_coalitions(game):
         for S in filter(lambda s: s | T == T, all_coalitions(game)):
             for i in filter(lambda i: i not in T, grand_coalition(game).players):
+                if isinstance(game, IncompleteGame):
+                    if any([
+                        not game.is_value_known(S | Coalition.from_players({i})),
+                        not game.is_value_known(T | Coalition.from_players({i})),
+                        not game.is_value_known(S),
+                        not game.is_value_known(T),
+                    ]):
+                        continue
+
                 lhs = game.get_value(S | Coalition.from_players({i})) - game.get_value(S)
                 rhs = game.get_value(T | Coalition.from_players({i})) - game.get_value(T)
                 if lhs > rhs:
