850af2f82732c7bf54d41f4031416d60ba51cba2
Filip <filip.uradnik9@gmail.com>
2024-01-27 22:25:52 +0100

    Add check for supermodular divergence on factory with expl.

---
+++ scripts/factory_divergence_check.py
@@ -0,0 +1,30 @@
+"""Check for what percentage of games is the divergence supermodular."""
+import sys
+
+from incomplete_cooperative.bounds import compute_bounds_superadditive
+from incomplete_cooperative.game import IncompleteCooperativeGame
+from incomplete_cooperative.generators import factory_generator
+from incomplete_cooperative.meta_game import MetaGame
+from incomplete_cooperative.exploitability import compute_exploitability
+from incomplete_cooperative.supermodularity_check import (
+    check_failed_diagnostics, check_supermodularity)
+
+divergence = compute_exploitability
+
+
+def main() -> None:
+    """Check the divergence for supermodularity."""
+    number_of_players = int(sys.argv[1])
+    game = factory_generator(number_of_players)
+    incomplete = IncompleteCooperativeGame(number_of_players, compute_bounds_superadditive)
+    meta_game = MetaGame(game, incomplete, divergence)
+    res = check_supermodularity(meta_game)
+    if res is None:
+        print("supermodular")
+    else:
+        print("not supermodular")
+        check_failed_diagnostics(meta_game, *res)
+
+
+if __name__ == '__main__':
+    main()
