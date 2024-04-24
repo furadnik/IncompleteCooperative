fix: use the current RM strategy, instead of the average

incomplete_cooperative/regret.py
@@ -207,12 +207,8 @@ class GameRegretMinimizer:
             next_coalition_pids = list(metacoalition_coal.inverted(self.number_of_coalitions).players)
             next_metacoalitions = [(metacoalition_coal + x).id for x in next_coalition_pids]
             next_metacoalition_ranks = self.meta_id_to_rank[next_metacoalitions]
-            if np.any(self.cumulative_strategy[i, next_coalition_pids]):
-                normalized_cumulative_strat = self.cumulative_strategy[i, next_coalition_pids] / \
-                    np.sum(self.cumulative_strategy[i, next_coalition_pids])
-            else:
-                normalized_cumulative_strat = np.ones(len(next_coalition_pids)) / len(next_coalition_pids)
-            node_reach_probability[next_metacoalition_ranks] += node_reach_probability[i] * normalized_cumulative_strat
+            regret_matching_strategy = self.regret_matching_strategy(int(metacoalition))[next_coalition_pids]
+            node_reach_probability[next_metacoalition_ranks] += node_reach_probability[i] * regret_matching_strategy
 
         # go up the tree, computing the regret and new cumulative strategy
         for i in range(self.number_of_regret_minimizers - 1, -1, -1):