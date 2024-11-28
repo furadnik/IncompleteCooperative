2e795a20985d2ccd83db7610edec928b0632c7f5
Filip Úradník <filip.uradnik9@gmail.com>
2024-11-28 20:56:28 +0100

    fix: add tolerance for superadditive test

--- incomplete_cooperative/game_properties.py
+++ incomplete_cooperative/game_properties.py
@@ -5,13 +5,18 @@ from .coalition_ids import all_coalitions, sub_coalitions
 from .protocols import Game
 
 
-def is_superadditive(game: Game) -> bool:
+def is_superadditive(game: Game, rtol=1e-9, atol=0) -> bool:
     """Test that `game` is superadditive."""
     values = game.get_values()
     for U in all_coalitions(game.number_of_players):
         Ss = sub_coalitions(U, game.number_of_players)
         Ts = U - Ss
-        if not np.all(values[Ss] + values[Ts] <= values[U]):
+        lhs = values[Ss] + values[Ts]
+        if not np.all(np.logical_or(
+                (lhs <= values[U]),
+                # if the values are equal, there could be rounding errors
+                np.isclose(lhs, values[U], rtol=rtol, atol=atol)
+        )):
             return False
     return True
 
