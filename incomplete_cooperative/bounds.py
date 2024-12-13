fix: sam bounds computation algorithm

incomplete_cooperative/bounds.py
@@ -14,8 +14,12 @@ from .protocols import BoundableIncompleteGame, GameBoundsComputer
 
 def compute_bounds_superadditive(game: BoundableIncompleteGame) -> None:
     """Compute the bounds given a superadditive incomplete game."""
+    assert game.is_value_known(Coalition(2**game.number_of_players - 1))
+    assert game.is_value_known(Coalition(0))
+    assert all(game.is_value_known(Coalition.from_players([i])) for i in range(game.number_of_players))
     for coalition in sorted(filter(lambda x: not game.is_value_known(x), all_coalitions(game)), key=len):
         sub_coalitions = [x for x in get_sub_coalitions(coalition) if x != coalition and x != Coalition(0)]
+        assert sub_coalitions
         complementary_coalitions = [coalition - x for x in sub_coalitions]
         lower_bound = np.max(game.get_lower_bounds(sub_coalitions) + game.get_lower_bounds(complementary_coalitions))
         game.set_lower_bound(lower_bound, coalition)
@@ -23,6 +27,7 @@ def compute_bounds_superadditive(game: BoundableIncompleteGame) -> None:
     for coalition in filter(lambda x: not game.is_value_known(x), all_coalitions(game)):
         known_super_coalitions = [x for x in get_super_coalitions(coalition, game.number_of_players)
                                   if game.is_value_known(x)]
+        assert known_super_coalitions
         complementary_coalitions = [x - coalition for x in known_super_coalitions]
         upper_bound = np.min(game.get_values(known_super_coalitions) - game.get_lower_bounds(complementary_coalitions))
         game.set_upper_bound(upper_bound, coalition)
@@ -84,12 +89,13 @@ def compute_bounds_superadditive_monotone_approx_cached(game: BoundableIncomplet
                 sub_coalitions = all_coalitions[
                     np.logical_or(coal_structure[coalition] == 1, coal_structure[coalition] == 0)
                 ]
+            assert i > 0 or coalition not in sub_coalitions
             complementary_coalitions = coalition ^ sub_coalitions
             lower_bound = np.max(game.get_lower_bounds()[sub_coalitions] + game.get_lower_bounds()[complementary_coalitions])
             game.set_lower_bound(lower_bound, Coalition(coalition))
         for coalition in unknown_sorted:
-            sub_coalitions = all_coalitions[np.logical_or(coal_structure[coalition] == 2, coal_structure[coalition] == 0)]
-            lower_bound = np.max(game.get_lower_bounds()[sub_coalitions])
+            super_coalitions = all_coalitions[np.logical_or(coal_structure[coalition] == 2, coal_structure[coalition] == 0)]
+            lower_bound = np.max(game.get_lower_bounds()[super_coalitions])
             game.set_lower_bound(lower_bound, Coalition(coalition))
 
     for coalition in unknown_sorted:

scripts/test_sam_bounds.py
@@ -12,49 +12,48 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-from incomplete_cooperative.bounds import \
-    compute_bounds_superadditive_monotone_approx_cached
+from incomplete_cooperative.bounds import (
+    compute_bounds_superadditive,
+    compute_bounds_superadditive_monotone_approx_cached)
 from incomplete_cooperative.coalitions import (Coalition, all_coalitions,
                                                minimal_game_coalitions)
 from incomplete_cooperative.functoolz import powerset
 from incomplete_cooperative.game import IncompleteCooperativeGame
 from incomplete_cooperative.game_properties import is_superadditive
-from incomplete_cooperative.generators import (covg_fn_generator,
-                                               k_budget_generator)
+from incomplete_cooperative.generators import k_budget_generator
 from incomplete_cooperative.norms import l1_norm
 
 mpl.use('TkAgg')
-NUM_PL = int(getenv("NUMBER_OF_PLAYERS") or 10)
-NUM = int(getenv("NUMBER_OF_REPET") or 10)
-STEP = int(getenv("STEP") or 10)
+NUM_PL = int(getenv("NUMBER_OF_PLAYERS") or 4)
+NUM = int(getenv("NUMBER_OF_REPET") or 1)
+STEP = int(getenv("STEP") or 0)
 OUTPUT = Path(getenv("SAVE_PATH") or ".")
 REPEATS = int(getenv("BOUND_REPEATS") or 10)
 SEED = int(getenv("SEED") or 42)
-BOUNDS = [partial(compute_bounds_superadditive_monotone_approx_cached, repetitions=2**i) for i in range(11)]
+BOUNDS = [compute_bounds_superadditive] + [partial(compute_bounds_superadditive_monotone_approx_cached, repetitions=i) for i in range(REPEATS)]
 
 
-GAMES = [covg_fn_generator(NUM_PL) for _ in range(NUM)]
+GAMES = [k_budget_generator(NUM_PL) for _ in range(NUM)]
 
 
 def try_bounds():
     """Run a random game, revealing values and computing the bounds."""
-    cum_norms = [0] * REPEATS
+    cum_norms = np.zeros(len(BOUNDS))
     for game in GAMES:
         min_game = list(minimal_game_coalitions(NUM_PL))
         incomplete_game = IncompleteCooperativeGame(NUM_PL)
         incomplete_game.set_known_values(game.get_values(min_game), min_game)
 
-        for step in range(min(STEP, 2**NUM_PL - len(min_game))):
-            unknown = np.arange(2**NUM_PL)[np.logical_not(incomplete_game.are_values_known())]
-            random_ch = np.random.choice(unknown)
-            incomplete_game.reveal_value(game.get_value(Coalition(random_ch)), Coalition(random_ch))
-        for repeat in range(REPEATS):
-            bounds = partial(
-                compute_bounds_superadditive_monotone_approx_cached, repetitions=repeat)
+        # for step in range(min(STEP, 2**NUM_PL - len(min_game))):
+        #     unknown = np.arange(2**NUM_PL)[np.logical_not(incomplete_game.are_values_known())]
+        #     random_ch = np.random.choice(unknown)
+        #     incomplete_game.reveal_value(game.get_value(Coalition(random_ch)), Coalition(random_ch))
+        for repeat, bounds in enumerate(BOUNDS):
             incomplete_game._bounds_computer = bounds
             incomplete_game.compute_bounds()
+            print(incomplete_game.get_upper_bounds())
             cum_norms[repeat] += float(l1_norm(incomplete_game))
-    avg_norms = np.array(cum_norms) / NUM
+    avg_norms = cum_norms / NUM
     plt.plot(avg_norms)
     plt.show()
 