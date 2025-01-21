6e0cdf7e98e4f8f025e371e9161cb2c23538ff3b
Filip Úradník <filip.uradnik9@gmail.com>
2025-01-21 18:43:14 +0100

    feat: add worst greedy alg

--- incomplete_cooperative/solvers/__init__.py
+++ incomplete_cooperative/solvers/__init__.py
@@ -1,4 +1,6 @@
 """Custom solvers module."""
+from functools import partial
+
 from incomplete_cooperative.protocols import Solver
 
 from .greedy import GreedySolver
@@ -7,6 +9,7 @@ from .random import RandomSolver
 
 SOLVERS: dict[str, type[Solver]] = {
     "greedy": GreedySolver,
+    "greedy_worst": partial(GreedySolver, worst=True),  # type: ignore[dict-item]
     "random": RandomSolver,
     "largest": LargestSolver,
 }
