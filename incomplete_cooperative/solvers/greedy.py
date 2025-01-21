095c9ab68a32eab4712936932def89edaac5742f
Filip Úradník <filip.uradnik9@gmail.com>
2025-01-21 18:43:14 +0100

    feat: add worst greedy alg

--- incomplete_cooperative/solvers/greedy.py
+++ incomplete_cooperative/solvers/greedy.py
@@ -6,8 +6,9 @@ from ..run.model import ModelInstance
 class GreedySolver:
     """Greedy solver implementation."""
 
-    def __init__(self, instance: ModelInstance | None = None) -> None:
+    def __init__(self, instance: ModelInstance | None = None, worst: bool = False) -> None:
         """Do nothing with the instance."""
+        self.worst = worst
 
     def _next_action_value(self, gym: Gym, action: int) -> Value:
         """Return a value of the next step."""
@@ -19,6 +20,6 @@ class GreedySolver:
         """Get the locally best next move."""
         valid_actions = [x for x in range(gym.action_masks().shape[0]) if gym.action_masks()[x]]
         action_values = [self._next_action_value(gym, act) for act in valid_actions]
-        max_action_value = max(action_values)  # type: ignore[type-var]
+        max_action_value = max(action_values) if not self.worst else min(action_values)  # type: ignore[type-var]
         best_actions = (act for act, val in zip(valid_actions, action_values) if val == max_action_value)
         return next(best_actions)
