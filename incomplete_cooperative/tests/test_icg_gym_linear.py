b123a1931db0e5180091c8c6aa708d0b12756662
Filip Úradník <filip.uradnik9@gmail.com>
2025-01-21 18:32:37 +0100

    fix: rename everywhere

--- incomplete_cooperative/tests/test_icg_gym_linear.py
+++ incomplete_cooperative/tests/test_icg_gym_linear.py
@@ -14,12 +14,12 @@ class TestICGGymLinear(TestCase, GymMixin):
         icg_gym = self.get_gym()
         icg_gym_wrapper = ICG_Gym_Linear(icg_gym)
         self.assertEqual(icg_gym, icg_gym_wrapper.icg_gym)
-        self.assertEqual(icg_gym.incomplete_game.number_of_players, icg_gym_wrapper.N)
+        self.assertEqual(icg_gym.incomplete_game.number_of_players, icg_gym_wrapper.number_of_players)
 
     def test_action_masks(self):
         icg_gym = self.get_gym()
         icg_gym_wrapper = ICG_Gym_Linear(icg_gym)
-        self.assertEqual(icg_gym_wrapper.action_masks().shape, (icg_gym_wrapper.N,))
+        self.assertEqual(icg_gym_wrapper.action_masks().shape, (icg_gym_wrapper.number_of_players,))
         self.assertEqual(icg_gym_wrapper.action_masks().dtype, bool)
 
     def test_cannot_reveal_coalition_of_size_one(self):
@@ -31,14 +31,14 @@ class TestICGGymLinear(TestCase, GymMixin):
         icg_gym = self.get_gym()
         icg_gym_wrapper = ICG_Gym_Linear(icg_gym)
         state, info = icg_gym_wrapper.reset()
-        self.assertEqual(state.shape, (icg_gym_wrapper.N,))
+        self.assertEqual(state.shape, (icg_gym_wrapper.number_of_players,))
         self.assertFalse(icg_gym_wrapper.done)
         self.assertEqual(info, {"game": icg_gym.full_game})
 
     def test_step(self):
         icg_gym = self.get_gym()
         icg_gym_wrapper = ICG_Gym_Linear(icg_gym)
-        num_pairs = icg_gym_wrapper.N * (icg_gym_wrapper.N - 1) // 2  # There are n choose 2 coalitions of size two
+        num_pairs = icg_gym_wrapper.number_of_players * (icg_gym_wrapper.number_of_players - 1) // 2  # There are n choose 2 coalitions of size two
         for i in range(num_pairs):
             self.assertTrue(icg_gym_wrapper.action_masks()[2],
                             f'Cannot reveal any more coalitions of size 2 after {i} steps')
