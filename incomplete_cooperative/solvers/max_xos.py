bf3aca43c7c51f1ee24b2daf3ef6bd192979c72a
Martin Černý <martesing@gmail.com>
2025-01-31 16:07:19 +0100

    Create max_xos.py

---
+++ incomplete_cooperative/solvers/max_xos.py
@@ -0,0 +1,39 @@
+"""A greedy using Max Xos Approximation Algorithm."""
+
+from ..protocols import Gym
+from ..run.model import ModelInstance
+from ..multiplicative import MaxXosApproximation
+
+import numpy as np
+
+
+class MaxXosSolver:
+    """Solve by Max XOS Approximation."""
+
+    def __init__(self, instance: ModelInstance | None = None) -> None:
+        """Precompute the Max XOS Approximation."""
+        icg_gym = instance.get_env()
+
+        inverted_game = icg_gym.incomplete_game
+        inverted_game.values = icg_gym.incomplete_game.values * -1 # The approximation algorithm needs submodular game
+
+        self.max_xos = MaxXosApproximation(inverted_game)
+
+        self.num_of_queries = len(self.max_xos.queried_coalitions) # This is stored so we can later learn when to stop plotting the graph
+
+        # Compute the rest of the coalitions and shuffle them
+        all_coalitions = np.arange(1, 2**icg_gym.incomplete_game.number_of_players)
+        filtered_coalitions = all_coalitions[~np.isin(all_coalitions, self.max_xos.queried_coalitions)]
+        self.remaining_coalitions = np.random.shuffle(filtered_coalitions)
+        
+
+    def next_step(self, gym: Gym) -> int:
+        """Get the locally best next move."""
+        if self.max_xos.queried_coalitions.size > 0:
+            popped_coalition = self.max_xos.queried_coalitions[-1]
+            self.max_xos.queried_coalitions = self.max_xos.queried_coalitions[:-1]
+            return popped_coalition
+        else:
+            popped_coalition = self.remaining_coalitions[-1]
+            self.remaining_coalitions = self.remaining_coalitions[:-1]
+            return popped_coalition
