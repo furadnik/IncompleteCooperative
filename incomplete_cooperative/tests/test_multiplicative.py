2706d7ac18c554a5dfb7486ee25bea2924df49d1
Filip Úradník <filip.uradnik9@gmail.com>
2025-02-07 17:09:49 +0100

    fix: extend test to proper game classes for each approximator

--- incomplete_cooperative/tests/test_multiplicative.py
+++ incomplete_cooperative/tests/test_multiplicative.py
@@ -3,7 +3,8 @@ import numpy as np
 import pytest
 
 from incomplete_cooperative.coalitions import player_to_coalition
-from incomplete_cooperative.generators import xos
+from incomplete_cooperative.generators import (covg_fn_generator,
+                                               k_budget_generator, xos)
 from incomplete_cooperative.multiplicative.max_xos_approximation import \
     compute_max_xos_approximation
 from incomplete_cooperative.multiplicative.rla_approximation import \
@@ -12,9 +13,14 @@ from incomplete_cooperative.multiplicative.rla_approximation import \
 
 @pytest.mark.parametrize("rep", range(10))
 @pytest.mark.parametrize("players", range(4, 10))
-@pytest.mark.parametrize("approximator", [compute_rla_approximation, compute_max_xos_approximation])
-def test_is_lower_bound(rep, approximator, players):
-    game = xos(players, normalize=False, normalize_additive=False)
+@pytest.mark.parametrize("approximator_generator", [
+    (compute_max_xos_approximation, xos),
+    (compute_rla_approximation, covg_fn_generator),
+    (compute_rla_approximation, k_budget_generator),
+])
+def test_is_lower_bound(rep, approximator_generator, players):
+    approximator, generator = approximator_generator
+    game = generator(players)
 
     singleton_values = -game.get_values([player_to_coalition(player) for player in range(players)])
     # we need to generate a game with all singletons bigger than 1
