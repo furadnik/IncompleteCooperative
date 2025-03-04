a892703c9d06a6c048f54f6eb91e65db49eef990
Filip Úradník <filip.uradnik9@gmail.com>
2025-03-04 18:56:02 +0100

    fix 411: input via modelinstance, use argparse

--- scripts/compare_multiplicative.py
+++ scripts/compare_multiplicative.py
@@ -1,7 +1,9 @@
 """This is a script for comparing the algorithms for multiplicative estimation with our approach."""
+import argparse
 import signal
 import sys
 from os import getenv
+from pathlib import Path
 
 import numpy as np
 import scipy
@@ -14,37 +16,43 @@ from incomplete_cooperative.generators import GENERATORS
 from incomplete_cooperative.multiplicative import APPROXIMATORS
 from incomplete_cooperative.multiplicative.multiplicative_factor import (
     mul_factor_lower_upper_bound, mul_factor_upper_to_approximation)
-
-OUR_ACTION_SEQUENCE: list[Coalition] = [Coalition(int(x))
-                                        for x in (getenv("ACTION_SEQUENCE") or "").split(",")
-                                        ] if getenv("ACTION_SEQUENCE") else []
-GAME_GENERATOR = GENERATORS[getenv("GAME_CLASS") or "k_budget_generator"]
-NUMBER_OF_PLAYERS = int(getenv("NUMBER_OF_PLAYERS") or 6)
-BOUNDS_COMPUTER = BOUNDS[getenv("BOUNDS_COMPUTER") or "sam_apx_1"]
-APPROXIMATOR = APPROXIMATORS[getenv("APPROXIMATOR") or "max_xos"]
-RNG = np.random.default_rng(int(getenv("SEED") or 42))
+from incomplete_cooperative.run.save import Output
 
 
-def main(out_data: list[tuple[float, ...]], samples: int | None = None) -> None:
+def get_action_sequence(model_path: Path, name: str) -> list[Coalition]:
+    """Get the action sequence from the model."""
+    output = Output.from_file(model_path, name)
+    return [Coalition(int(action)) for action in output.actions[:, 0]]
+
+
+def main(out_data: list[tuple[float, ...]], args: argparse.Namespace) -> None:
     """Compare multiplicative algos to ours."""
-    incomplete_game = IncompleteCooperativeGame(NUMBER_OF_PLAYERS, BOUNDS_COMPUTER)
-    minimal_game = list(minimal_game_coalitions(NUMBER_OF_PLAYERS))
+    game_generator = GENERATORS[args.game_generator]
+    bounds_computer = BOUNDS[args.bounds_computer]
+    approximator = APPROXIMATORS[args.approximator]
+    samples = args.samples
+    rng = np.random.default_rng(args.seed)
+    number_of_players = args.number_of_players
+    our_action_sequence = get_action_sequence(args.model, args.model_name)
+
+    incomplete_game = IncompleteCooperativeGame(number_of_players, bounds_computer)
+    minimal_game = list(minimal_game_coalitions(number_of_players))
     print(minimal_game)
 
     print("sample", "len_queried", "approx_to_upper", "lower_to_upper_theirs", "lower_to_upper_ours")
     sample = 0
     while samples is None or sample < samples:
-        game = GAME_GENERATOR(NUMBER_OF_PLAYERS, RNG)
+        game = game_generator(number_of_players, rng)
 
         # their approximation
-        queried_coal_ids, approximated_game = APPROXIMATOR(-game)
+        queried_coal_ids, approximated_game = approximator(-game)
         approximated_game = -approximated_game
         pure_queried_coals = set(map(Coalition, map(int, queried_coal_ids))).difference(minimal_game)
         queried_coals = list(pure_queried_coals.union(minimal_game))
         budget = len(pure_queried_coals)  # only count those which weren't in the minimal game
 
         # compute our ratio
-        revealed_sequence = OUR_ACTION_SEQUENCE[:budget]
+        revealed_sequence = our_action_sequence[:budget]
         incomplete_game.set_known_values(game.get_values(minimal_game + revealed_sequence),
                                          minimal_game + revealed_sequence)
         incomplete_game.compute_bounds()
@@ -77,5 +85,15 @@ def _callback_fn(a=None, b=None) -> None:
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Compare multiplicative algorithms.')
+    parser.add_argument('samples', type=int, nargs='?', default=None)
+    parser.add_argument('--game_generator', type=str, default="k_budget_generator")
+    parser.add_argument('--number_of_players', type=int, default=6)
+    parser.add_argument('--bounds_computer', type=str, default="sam_apx_1")
+    parser.add_argument('--approximator', type=str, default="max_xos")
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--model', type=Path)
+    parser.add_argument('--model_name', type=str, default="expected_greedy")
+
     signal.signal(signal.SIGINT, _callback_fn)
-    main(out_data, int(sys.argv[1]) if len(sys.argv) > 1 else None)
+    main(out_data, parser.parse_args())
